65dbd2f13810b99e239c8cad38930801024c0db7
mt-shke <81960831+mt-shke@users.noreply.github.com>
2021-10-12 08:34:55 +0200

    Delete ready.js

--- start/src/ready.js
+++
@@ -1,9 +0,0 @@
-function Ready() {
-	return (
-		<div>
-			<h3>Il était une fois,</h3> <p> de gros ksos qui n'avaient rien d'autre à faire de leur life</p>
-		</div>
-	);
-}
-
-export default Ready;
